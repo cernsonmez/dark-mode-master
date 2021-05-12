78941c7b0a2c3a4227621f07aca6823e3e770b26
Ceren Sönmez <cerensonmez95@gmail.com>
2021-05-12 14:22:41 +0300

    Dark mode switch

--- app/src/main/java/com/csonmez/darkmode/MainActivity.kt
+++ app/src/main/java/com/csonmez/darkmode/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.csonmez.darkmode
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.csonmez.darkmode.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        controlNightMode()
+
+        viewBinding.darkModeSwitch.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { compoundButton, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        })
+    }
+
+    /**
+     *  Controls the night theme is active for device. If it activated the switch value set as checked.
+     **/
+    private fun controlNightMode(){
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            viewBinding.darkModeSwitch.setChecked(true)
+        }
     }
 }
